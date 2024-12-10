3646343e3359f67b5ba5c0e5193a9ebf28653f61
vimukthiwaththegama <vwaththegama@gmail.com>
2024-12-10 20:13:41 +0530

    10/12/24-1

--- RealTimeEventTicketing/src/main/java/com/cw_oop/RealTimeEventTicketing/backend/controller/ConfigurationController.java
+++ RealTimeEventTicketing/src/main/java/com/cw_oop/RealTimeEventTicketing/backend/controller/ConfigurationController.java
@@ -1,4 +1,29 @@
 package com.cw_oop.RealTimeEventTicketing.backend.controller;
 
+import com.cw_oop.RealTimeEventTicketing.backend.entity.Config;
+import com.cw_oop.RealTimeEventTicketing.backend.service.ConfigService;
+import com.cw_oop.RealTimeEventTicketing.cli.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/config")
 public class ConfigurationController {
+
+    @Autowired
+    private ConfigService configService;
+
+    @PostMapping("/add")
+    public void setConfiguration(@RequestBody Config config) {
+        configService.saveToFile(config);
+        Configuration configuration = new Configuration(config.getTotalTickets(),
+                                                        config.getTicketReleaseRate(),
+                                                        config.getTicketsRetrievalRate(),
+                                                        config.getMaxPoolCapacity());
+    }
+//    @GetMapping("/get-config")
+//    public Config getConfiguration() {
+//
+//    }
+
 }
